043a6de661b64378e22e0c55406e3918d40d27e4
Eduard Brukish <eduardbrukish@gmail.com>
2024-01-24 12:18:34 +0200

    feat(develop): add read file and read directory logic

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { readdir } from 'fs/promises'
+import { join } from 'path'
+import { getDirname } from '../helpers/dirnameHelper.js'
+
 const list = async () => {
-    // Write your code here 
+    const fileDirectory = getDirname(import.meta.url)
+
+    try {
+        const directoryPathToReadFiles = join(fileDirectory, 'files')
+
+        const files = await readdir(directoryPathToReadFiles)
+        for (const file of files) {
+            console.log(file)
+        }
+    } catch (e) {
+        throw new Error('FS operation failed')
+    }
 };
 
 await list();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import { readFile } from 'fs/promises'
+import { join } from 'path'
+import { getDirname } from '../helpers/dirnameHelper.js'
+
 const read = async () => {
-    // Write your code here 
+    const fileDirectory = getDirname(import.meta.url)
+
+    try {
+        const filePathToRead = join(fileDirectory, 'files', 'fileToRead.txt')
+
+        const content = await readFile(filePathToRead, {encoding: 'utf8'})
+        console.log(content)
+    } catch (e) {
+        throw new Error('FS operation failed')
+    }
 };
 
 await read();
