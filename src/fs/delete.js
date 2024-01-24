54ad519e02d0ff2ac77a567a56df13f917f7bcd5
Eduard Brukish <eduardbrukish@gmail.com>
2024-01-24 11:52:19 +0200

    feat(develop): add delete file logic

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import { rm } from 'fs/promises'
+import { join } from 'path'
+import { getDirname } from '../helpers/dirnameHelper.js'
+
 const remove = async () => {
-    // Write your code here 
+    const fileDirectory = getDirname(import.meta.url)
+    const filePathToDelete = join(fileDirectory, 'files', 'fileToRemove.txt')
+
+    try {
+        await rm(filePathToDelete)
+    } catch (e) {
+        throw new Error('FS operation failed')
+    }
 };
 
 await remove();
