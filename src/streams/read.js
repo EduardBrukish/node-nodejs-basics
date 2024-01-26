4eb77f7cbbe431a01c0d1155fb88ee6dcba8c58f
Eduard Brukish <eduardbrukish@gmail.com>
2024-01-26 12:27:42 +0200

    feat(develop): complete streams task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,26 @@
+import { createReadStream } from 'node:fs';
+import { join } from 'path'
+import { getDirname } from '../helpers/dirnameHelper.js'
+
 const read = async () => {
-    // Write your code here 
+    const fileDirectory = getDirname(import.meta.url)
+    const fileNameToRead = join(fileDirectory, 'files', 'fileToRead.txt')
+
+    const fileData = await new Promise((res) => {
+        let readStream = createReadStream(fileNameToRead, 'utf8')
+        let chunk = ''
+
+        readStream.on('data', (data) => {
+            chunk = chunk + data
+        })
+
+        readStream.on('close', () => {
+            res(chunk)
+        })
+
+    })
+    
+    process.stdout.write(fileData)
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { createWriteStream } from 'node:fs';
+import { join } from 'path'
+import { getDirname } from '../helpers/dirnameHelper.js'
+
 const write = async () => {
-    // Write your code here 
+    const fileDirectory = getDirname(import.meta.url)
+    const fileNameToWrite = join(fileDirectory, 'files', 'fileToWrite.txt')
+
+    const writeStream = createWriteStream(fileNameToWrite)
+
+    console.log('Type some text in terminal, press Enter and written text will appear in fileToWrite.txt file')
+
+    process.stdin.pipe(writeStream)
 };
 
 await write();
