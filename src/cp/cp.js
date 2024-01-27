2b01159bdacc7270baf244bad4f3d1bffc0c2b73
Eduard Brukish <eduardbrukish@gmail.com>
2024-01-27 21:33:15 +0200

    feat(develop): implement spawn logic

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import { spawn } from 'node:child_process'
+import { join } from 'path'
+import { getDirname } from '../helpers/dirnameHelper.js'
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    
+    const fileDirectory = getDirname(import.meta.url)
+    const fileToPathToSpawn = join(fileDirectory, 'files', 'script.js')
+    
+    const childProcess = spawn('node', [fileToPathToSpawn, ...args])
+
+    process.stdin.pipe(childProcess.stdin)
+    childProcess.stdout.pipe(process.stdout)
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['argumentOne', 'argumentTwo']);
